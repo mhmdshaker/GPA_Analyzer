5e1ed18560c5619724186580e807bf91785ae2c6
mhmdshaker <shaker.mohamad01@gmail.com>
2024-05-29 01:50:22 +0300

    chore: Add route to add grade for a course and user

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import render_template, request, jsonify
 from models.user import User, UserSchema
 from models.course import Course, CourseSchema
 import bcrypt
-from init import app, db, ma
+from init import app, db, ma, user_courses
 import jwt, datetime
 from db_config import SECRET_KEY
 
@@ -68,6 +68,36 @@ def search_courses():
     return jsonify(course_schema.dump(courses, many=True))
 
 # #add a grade to a course for a specific user:
+@app.route('/add_grade', methods = ['POST'])
+def add_grade():
+    course_name = request.json['course']
+    grade = request.json['grade']
+    semester = request.json['semester']
+    #to get the token from the session and get the email:
+    token = extract_auth_token(request)
+    user_email = None
+    if token:
+        try:
+            #to get the email from the token
+            email = decode_token(token)
+        except jwt.ExpiredSignatureError:
+            return jsonify({"message": "Token expired."}), 403
+        except jwt.InvalidTokenError:
+            return jsonify({"message": "Invalid token."}), 403
+    
+    # Get the course and user from the database
+    course = Course.query.filter_by(name=course_name).first()
+    user = User.query.filter_by(email=user_email).first()
+    if not course or not user:
+        return jsonify({'message': 'Course or user not found'}), 404
 
+    # Create a new CourseUser instance
+    course_user = user_courses(name=course.name, email=user.email, grade=grade, semester=semester)
+    # Add the new CourseUser to the database
+    db.session.add(course_user)
+    db.session.commit()
 
+    return jsonify({'message': 'Grade added successfully'}), 200
+        
+    
     
